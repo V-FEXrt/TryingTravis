a43d45df9a174c6aa4d4f3c84d29176d552bfe8e
V-FEXrt <ash123125@yahoo.com>
2015-07-09 22:21:43 -0500

    Break it

--- TryingTravis/ViewController.m
+++ TryingTravis/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"LOGS:");
+    NSLog(@"LOGS:") //break the code
     // Do any additional setup after loading the view, typically from a nib.
 }
 
